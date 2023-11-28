973233a69e750047c004aa2ffa34ed2ea4653161
Leon Nicolhas <leonnhsalas@gmail.com>
2023-11-28 09:29:15 -0600

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+microndas = "A023D4"
+
+horno = "POM56S"
+
+refrigerador = "458JM3"
+
+televisor = "Tl2309a"
+
+computadora = "Ce490n"
+
+kitdepintura = "P87nFb"
+
+cuboruby = "CM91nT"
+
+vaso = "Vv78k39"
+
+inodoro = "P1289s"
+
+papel = "Piu923e"
